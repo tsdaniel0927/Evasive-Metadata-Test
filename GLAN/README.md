5c2adca92652c2859bbb8a84e40ca7593a11430b
Daniel Tan <tsdaniel0927@gmail.com>
2022-10-07 16:52:07 +0800

    GLAN model testing

---
+++ GLAN/README.md
@@ -0,0 +1,40 @@
+# Paper of the source codes released:
+Chunyuan Yuan, Qianwen Ma, Wei Zhou, Jizhong Han, Songlin Hu. Jointly embedding the local and global relations of heterogeneous graph for rumor detection. In 19th IEEE International Conference on Data Mining, IEEE ICDM 2019.
+
+# Dependencies:
+Gensim==3.7.2
+
+Jieba==0.39
+
+Scikit-learn==0.21.2
+
+Pytorch==1.4.0
+
+# Datasets
+The main directory contains the directories of Weibo dataset and two Twitter datasets: twitter15 and twitter16. In each directory, there are:
+- twitter15.train, twitter15.dev, and twitter15.test file: This files provide traing, development and test samples in a format like: 'source tweet ID \t source tweet content \t label'
+  
+- twitter15_graph.txt file: This file provides the source posts content of the trees in a format like: 'source tweet ID \t userID1:weight1 userID2:weight2 ...'  
+
+These dastasets are preprocessed according to our requirement and original datasets can be available at https://www.dropbox.com/s/7ewzdrbelpmrnxu/rumdetect2017.zip?dl=0  (Twitter)  and http://alt.qcri.org/~wgao/data/rumdect.zip (Weibo).
+
+If you want to preprocess the dataset by youself, you can use the word2vec used in our work. The pretrained word2vec can be available at https://drive.google.com/drive/folders/1IMOJCyolpYtoflEqQsj3jn5BYnaRhsiY?usp=sharing.
+
+
+# Reproduce the experimental results:
+1. create an empty directory: checkpoint/
+2. run script run.py 
+
+
+## Citation
+If you find this code useful in your research, please cite our paper:
+```
+@inproceedings{rumor_yuan_2019,
+  title={Jointly embedding the local and global relations of heterogeneous graph for rumor detection},
+  author={Yuan, Chunyuan and Ma, Qianwen and Zhou, Wei and Han, Jizhong and Hu, Songlin},
+  booktitle={The 19th IEEE International Conference on Data Mining},
+  year={2019},
+  organization={IEEE}
+}
+```
+
---
+++ GLAN/run.py
@@ -0,0 +1,73 @@
+import os
+import pickle
+import torch
+from sklearn.metrics import classification_report
+from model.GLAN import GLAN
+
+os.environ["CUDA_VISIBLE_DEVICES"] = "-1"
+
+def load_dataset(task):
+    X_train_tid, X_train_source, X_train_replies, y_train, word_embeddings, graph = pickle.load(open("dataset/"+task+"/train.pkl", 'rb'))
+    X_dev_tid, X_dev_source, X_dev_replies, y_dev = pickle.load(open("dataset/"+task+"/dev.pkl", 'rb'))
+    X_test_tid, X_test_source, X_test_replies, y_test = pickle.load(open("dataset/"+task+"/test.pkl", 'rb'))
+    config['embedding_weights'] = word_embeddings
+    print("#nodes: ", graph.num_nodes)
+    return X_train_tid, X_train_source, X_train_replies, y_train, \
+           X_dev_tid, X_dev_source, X_dev_replies, y_dev, \
+           X_test_tid, X_test_source, X_test_replies, y_test, graph
+
+
+def train_and_test(model, task):
+    model_suffix = model.__name__.lower().strip("text")
+    config['save_path'] = 'checkpoint/weights.best.' + task + "." + model_suffix
+
+    X_train_tid, X_train_source, X_train_replies, y_train, \
+    X_dev_tid, X_dev_source, X_dev_replies, y_dev, \
+    X_test_tid, X_test_source, X_test_replies, y_test, graph = load_dataset(task)
+
+    nn = model(config, graph)
+    # nn.fit(X_train_tid, X_train_source, X_train_replies, y_train,
+    #        X_dev_tid, X_dev_source, X_dev_replies, y_dev)
+
+    print("================================")
+    nn.load_state_dict(torch.load(config['save_path']))
+    y_pred = nn.predict(X_test_tid, X_test_source, X_test_replies)
+    print(classification_report(y_test, y_pred, target_names=config['target_names'], digits=3))
+
+
+config = {
+    'lr':1e-3,
+    'reg':0,
+    'batch_size':16,
+    'nb_filters':100,
+    'kernel_sizes':[3, 4, 5],
+    'dropout':0.5,
+    'maxlen':50,
+    'epochs':30,
+    'num_classes':4,
+    'target_names':['NR', 'FR', 'UR', 'TR']
+}
+
+
+if __name__ == '__main__':
+    # task = 'twitter15'
+    # task = 'twitter16'
+    # task = 'weibo'
+    task = 'twitter_replies_all'
+    # task = 'twitter_replies_no'
+    # task = 'twitter_time_240'
+    # task = 'twitter_time_480'
+    # task = 'twitter_time_720'
+    # task = 'twitter_replies_swapped'
+    # task = 'twitter_random_td'
+    print("task: ", task)
+
+    if task == 'weibo':
+        config['num_classes'] = 2
+        config['batch_size'] = 64
+        config['reg'] = 1e-5
+        config['target_names'] = ['NR', 'FR']
+
+    model = GLAN
+    train_and_test(model, task)
+
